133f33d9fe11ab2e8e32a5ee7caccf0e13987e35
Veli Dönmez <vdonmez42@gmail.com>
2019-12-31 09:38:52 +0300

    some changed

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="calculator">
-    <p>this is the inner test</p>
+    <p>this is the inner test23</p>
     <div class="display">{{current || "0"}}</div>
     <div class="btn" @click="clear">C</div>
     <div class="btn" @click="sign">+/-</div>
